972c3f1646ae68525d2d46c84a2c37a15cb94aff
007AxelBerkati <bucel1916@gmail.com>
2023-05-10 23:40:46 +0700

    slicing : onBoardingOne

---
+++ app/src/main/java/com/crowcel/myapplication/onboarding/OnboardingOneActivity.kt
@@ -0,0 +1,11 @@
+package com.crowcel.myapplication.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_one)
+    }
+}
